7f133c42eea87276263dacb5359ad2a9c0bc4a84
Muhammad Tayyab <tayyabimo@gmail.com>
2025-04-06 18:27:01 +0500

    correction

--- client/src/pages/Learn.jsx
+++ client/src/pages/Learn.jsx
@@ -4,10 +4,12 @@ import HeroLearn from '../components/HeroLearn'
 import CategoriesLearn from '../components/CategoriesLearn'
 import PopularCourses from '../components/PopularCourses'
 import Footer from '../components/FooterLearn'
+import NavbarLearn from '../components/NavbarLearn'
 
 function Learn() {
   return (
     <div>
+      <NavbarLearn/>
       <HeroLearn />
       <CategoriesLearn />
       <PopularCourses />
